9f51208f1d0a121050ab8565b7deb67b681c59db
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-09-19 13:20:17 +0200

    feat: Add speech to text support

---
+++ externs/speech.js
@@ -0,0 +1,18 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for SpeechRecognition which were missing in the
+ * Closure compiler.
+ *
+ * @externs
+ */
+
+/**
+ * @type {function(?MediaStreamTrack=)}
+ * @override
+ */
+SpeechRecognition.prototype.start;
